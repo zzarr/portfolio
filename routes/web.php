5ad0eb10ea733e866a6cb8bdff66ae5d0552c03a
zzarr <azharsyah.curug@gmail.com>
2026-03-18 20:31:58 +0700

    add login route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
