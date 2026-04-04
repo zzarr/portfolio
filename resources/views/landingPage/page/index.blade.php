b8b32123f66335a4838ae12068994dd3caba628d
zzarr <azharsyah.curug@gmail.com>
2026-04-04 09:48:41 +0700

    add favicon

--- resources/views/landingPage/page/index.blade.php
+++ resources/views/landingPage/page/index.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>Portfolio</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('admin/assets/css/icons.css') }}">
